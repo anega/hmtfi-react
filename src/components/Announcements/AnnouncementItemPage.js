3ca2dd2e8d585a43c9e5f740bfef3061e68abe91
anegin <anegamega@gmail.com>
2017-04-12 18:39:09 +0300

    Announcement page: stage

--- src/components/Announcements/AnnouncementItemPage.js
+++ src/components/Announcements/AnnouncementItemPage.js
@@ -1,25 +1,50 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 
 class AnnouncementItemPage extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    static propTypes = {
+        announcement: PropTypes.object.isRequired
+    };
+
     componentWillReceiveProps(nextProps) {
-        console.log(3);
-        if (this.props.announcement.id != nextProps.announcement.id) {
+        if (this.props.announcement.id !== nextProps.announcement.id) {
             this.setState({announcement: Object.assign({}, nextProps.announcement)});
         }
     }
 
     render() {
-        console.log(4);
         return (
-            <h1>Item page</h1>
+            <div className="content">
+                <div className="gallery">
+                    <img src={this.props.announcement.preview_image} alt=""/>
+                    <div className="gallery-list">
+                        <ul>
+                            {this.props.announcement.images && this.props.announcement.images.map((image, index) => {
+                                return <li key={index}><img src={image} alt=""/></li>
+                            })}
+                        </ul>
+                    </div>
+                </div>
+                <div className="announcement-info">
+                    <button className="connect-user">Connect</button>
+                    <h2 className="item-title">{this.props.announcement.title}</h2>
+                    <div className="announcement-full-description">{this.props.announcement.about}</div>
+                </div>
+                <div className="announcement-map">
+                    <img src="./src/assets/images/test-map.jpg" alt=""/>
+                    <img className="map-pin" src="./src/assets/images/map-pin.png" alt=""/>
+                </div>
+            </div>
         );
     }
 }
 
 function getAnnouncementById(announcementList, id) {
-    console.log('qqqqq');
-    const currAnnouncement = announcementList.filter(announcement => announcement.id == id);
+    const currAnnouncement = announcementList.filter(announcement => announcement.id === id);
     if (currAnnouncement) {
         return currAnnouncement[0];
     }
@@ -27,21 +52,19 @@ function getAnnouncementById(announcementList, id) {
 }
 
 function mapStateToProps(state, ownProps) {
-    console.log(1);
-    let announcementId = ownProps.params.id;
+    let announcementId = +ownProps.params.id;
     let announcement = {};
     if (announcementId && state.announcementsList.length > 0) {
         announcement = getAnnouncementById(state.announcementsList, announcementId);
     }
-    console.log(2);
 
     return {
         announcement: announcement
     };
 }
 
-// function mapDispatchToProps(dispatch) {
-//     return {};
-// }
+function mapDispatchToProps(dispatch) {
+    return {};
+}
 
-export default connect(mapStateToProps)(AnnouncementItemPage);
+export default connect(mapStateToProps, mapDispatchToProps)(AnnouncementItemPage);
