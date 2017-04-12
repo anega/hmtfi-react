a9d3f8e307d5f7c0b0e73a582a40379a8856a97e
anegin <anegamega@gmail.com>
2017-04-12 19:12:28 +0300

    Add 404 page

--- src/router.js
+++ src/router.js
@@ -4,10 +4,12 @@ import { Route, IndexRoute } from 'react-router';
 import App from './components/App/index';
 import AnnouncementsList from './components/Announcements/index';
 import AnnouncementsItemPage from './components/Announcements/AnnouncementItemPage';
+import NotFound from './components/NotFound/index';
 
 export default (
     <Route path="/" component={App}>
         <IndexRoute component={AnnouncementsList}/>
         <Route path="announcement/:id" component={AnnouncementsItemPage}/>
+        <Route path="*" component={NotFound}/>
     </Route>
 );
